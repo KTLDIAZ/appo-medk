d8bb6eeebf4e52313c39e0fc508a84a194ac37a4
melvin diaz <melvindiaz99@gmail.com>
2021-12-08 15:10:24 -0600

    Elimina consultorio y centro de salud

--- src/app/pages/health-centers/health-centers.component.ts
+++ src/app/pages/health-centers/health-centers.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchService } from '../services/fetch.service';
 import { HealthCenter } from '../interfaces/pages.interface';
+import { DeleteService } from '../services/delete.service';
+import { AuthService } from '../../authentication/services/auth.service';
 
 @Component({
   selector: 'app-health-centers',
@@ -9,12 +11,26 @@ import { HealthCenter } from '../interfaces/pages.interface';
 })
 export class HealthCentersComponent implements OnInit {
   public healthCenters: HealthCenter[] = [];
+  public isAdmin: boolean = false;
 
-  constructor(private fetchService: FetchService) {}
+  constructor(
+    private fetchService: FetchService,
+    private deleteService: DeleteService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
+    this.isAdmin = this.authService.currentUser?.rol === 'ADMIN';
     this.fetchService
       .getHealthCenters()
       .subscribe((res) => (this.healthCenters = res));
+  }
+
+  deleteHealthCenter(id: string) {
+    this.deleteService.deleteHealthCenter(id).subscribe((res) => {
+      if (res === true) {
+        window.location.reload();
+      }
+    });
   }
 }
--- src/app/pages/offices/offices.component.ts
+++ src/app/pages/offices/offices.component.ts
@@ -3,6 +3,8 @@ import { FetchService } from '../services/fetch.service';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { Offices } from '../interfaces/pages.interface';
+import { AuthService } from '../../authentication/services/auth.service';
+import { DeleteService } from '../services/delete.service';
 
 @Component({
   selector: 'app-offices',
@@ -11,15 +13,27 @@ import { Offices } from '../interfaces/pages.interface';
 })
 export class OfficesComponent implements OnInit {
   public offices: Offices[] = [];
+  public isAdmin: boolean = false;
 
   constructor(
     private fetchService: FetchService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private deleteService: DeleteService,
+    private authService: AuthService
   ) {}
 
   ngOnInit(): void {
+    this.isAdmin = this.authService.currentUser?.rol === 'ADMIN';
     this.activatedRoute.params
       .pipe(switchMap(({ id }) => this.fetchService.getOffices(id)))
       .subscribe((res) => (this.offices = res));
+  }
+
+  deleteOffice(id: string) {
+    this.deleteService.deleteOffice(id).subscribe((res) => {
+      if (res === true) {
+        window.location.reload();
+      }
+    });
   }
 }
